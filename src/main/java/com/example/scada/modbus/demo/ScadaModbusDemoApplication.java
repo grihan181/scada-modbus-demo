461c6eb1368751009d4d486bb29966a0f3f7b599
Peter Merenkov <peter.merenkov@gmail.com>
2024-10-29 04:34:54 +0400

    Init

---
+++ src/main/java/com/example/scada/modbus/demo/ScadaModbusDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.scada.modbus.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScadaModbusDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScadaModbusDemoApplication.class, args);
+    }
+
+}
